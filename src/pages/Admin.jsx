d8d3a2c61d5707054b678d5ad641aa7b86dff4b0
EmersonJr <152315731+emersonjrdev@users.noreply.github.com>
2025-11-26 17:08:38 -0300

    fazendo carregar imagem localmente esquecii

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "https://lunabe-store.onrender.com/products";
+const API_URL = "https://lunabe-store.onrender.com/api/products";
 
 const Admin = () => {
   const [loggedIn, setLoggedIn] = useState(false);
